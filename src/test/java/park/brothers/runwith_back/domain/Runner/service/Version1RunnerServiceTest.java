734f099aad31b45954da97cbfe15f89b6ac10c3d
Euizzang8001 <qkrdmlcks55@korea.ac.kr>
2026-02-13 22:32:18 +0900

    [test] Add RunnerServiceTest - save success test - save fail test - duplication - check duplication success test

---
+++ src/test/java/park/brothers/runwith_back/domain/Runner/service/Version1RunnerServiceTest.java
@@ -0,0 +1,141 @@
+package park.brothers.runwith_back.domain.Runner.service;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import park.brothers.runwith_back.domain.Belong.entity.Belong;
+import park.brothers.runwith_back.domain.Belong.repository.BelongRepository;
+import park.brothers.runwith_back.domain.Group.entity.Group;
+import park.brothers.runwith_back.domain.Group.repository.GroupRepository;
+import park.brothers.runwith_back.domain.Runner.dto.Request.CreateRunnerRequestDto;
+import park.brothers.runwith_back.domain.Runner.dto.Response.CreateRunnerResponseDto;
+import park.brothers.runwith_back.domain.Runner.entity.Runner;
+import park.brothers.runwith_back.domain.Runner.repository.RunnerRepository;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+class Version1RunnerServiceTest {
+    @InjectMocks
+    private Version1RunnerService runnerService;
+
+    @Mock
+    private RunnerRepository runnerRepository;
+
+    @Mock
+    private GroupRepository groupRepository;
+
+    @Mock
+    private BelongRepository belongRepository;
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    @DisplayName("러너 저장 서비스 성공 테스트")
+    void save() {
+        //given
+        //service 입력값
+        CreateRunnerRequestDto createRunnerRequestDto = new CreateRunnerRequestDto(
+          "test_name",
+          "test_email",
+          "test_password",
+          "test_imageLink"
+        );
+
+        //가짜 러너 생성
+        Runner mockRunner = new Runner();
+        mockRunner.setId(1L);
+        mockRunner.setName("test_name");
+        mockRunner.setPassword("test_password");
+        mockRunner.setEmail("test_email");
+        mockRunner.setImageLink("test_imageLink");
+
+        //이름 중복 없다고 가정
+        given(runnerRepository.findByName(anyString())).willReturn(Optional.empty());
+        //러너 저장 시, 러너 올바르게 반환
+        given(runnerRepository.save(any(Runner.class))).willReturn(mockRunner);
+        // 그룹 저장 시, 그룹 객체 반환
+        given(groupRepository.save(any(Group.class))).willReturn(new Group());
+        // 빌롱 저장 시, 빌롱 객체 반환
+        given(belongRepository.save(any(Belong.class))).willReturn(new Belong());
+
+        //when
+        CreateRunnerResponseDto fakeResponse = runnerService.save(createRunnerRequestDto);
+
+        //then
+        assertThat(fakeResponse).isNotNull(); //응답값은 null이면 안됨
+        //응답값은 아래 값들을 가져야만 한다.
+        assertThat(fakeResponse.getName()).isEqualTo("test_name");
+        assertThat(fakeResponse.getEmail()).isEqualTo("test_email");
+        assertThat(fakeResponse.getImageLink()).isEqualTo("test_imageLink");
+        //repository.save() 코드들은 1번식만 실행되어야 한다.
+        verify(runnerRepository, times(1)).save(any(Runner.class));
+        verify(groupRepository, times(1)).save(any(Group.class));
+        verify(belongRepository, times(1)).save(any(Belong.class));
+    }
+
+    @Test
+    @DisplayName("러너 저장 실패 테스트 - 이미 존재하는 이름")
+    void saveFail(){
+        //given
+        //service 입력값
+        CreateRunnerRequestDto createRunnerRequestDto = new CreateRunnerRequestDto(
+                "test_name",
+                "test_email",
+                "test_password",
+                "test_imageLink"
+        );
+
+        //이름으로 러너 찾기 repository 수행 시, 러너 객체 반환(이미 있음)
+        given(runnerRepository.findByName(anyString())).willReturn(Optional.of(new Runner()));
+
+        //then & when
+        //IllegalAccessError발생해야 함.
+        assertThrows(IllegalAccessError.class, () -> {
+            runnerService.save(createRunnerRequestDto);
+        });
+        // 저장 코드는 수행되어선 안됨.
+        verify(runnerRepository, times(0)).save(any(Runner.class));
+        verify(groupRepository, times(0)).save(any(Group.class));
+        verify(belongRepository, times(0)).save(any(Belong.class));
+    }
+
+    @Test
+    @DisplayName("중복 성공 테스트")
+    void checkDuplication() {
+        //given
+        //서비스 입력값
+        CreateRunnerRequestDto createRunnerRequestDto = new CreateRunnerRequestDto(
+                "test_name",
+                "test_email",
+                "test_password",
+                "test_imageLink"
+        );
+
+        //중복값이 있다고 가정
+        given(runnerRepository.checkDuplication(anyString(), anyString())).willReturn(true);
+
+        //when & then
+        //중복값이 있다고 제대로 잘 나오는지 확인
+        assertThat(runnerService.checkDuplication(createRunnerRequestDto)).isTrue();
+    }
+}
