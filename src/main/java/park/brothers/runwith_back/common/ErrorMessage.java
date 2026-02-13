ebfa49f023d892fb8f5c5dd25865b8cd502a8e6e
Euizzang8001 <qkrdmlcks55@korea.ac.kr>
2026-02-13 18:20:48 +0900

    [test] Add RunnerControllerTest - successful save test - duplication test

---
+++ src/main/java/park/brothers/runwith_back/common/ErrorMessage.java
@@ -0,0 +1,3 @@
+package park.brothers.runwith_back.common;
+
+public record ErrorMessage(String message) {};
--- src/main/java/park/brothers/runwith_back/domain/Runner/dto/Request/CreateRunnerRequestDto.java
+++ src/main/java/park/brothers/runwith_back/domain/Runner/dto/Request/CreateRunnerRequestDto.java
@@ -1,11 +1,13 @@
 package park.brothers.runwith_back.domain.Runner.dto.Request;
 
 import jakarta.validation.constraints.NotEmpty;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.Getter;
 
 @Data
 @Getter
+@AllArgsConstructor
 public class CreateRunnerRequestDto {
     @NotEmpty
     private String name;
