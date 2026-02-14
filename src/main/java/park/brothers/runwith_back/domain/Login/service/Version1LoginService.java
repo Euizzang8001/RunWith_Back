9912a4d738635bcbfa8bbd37049ca0f805f648b0
Euizzang8001 <qkrdmlcks55@korea.ac.kr>
2026-02-14 15:55:34 +0900

    [refactor] divide dto to request and response

--- src/main/java/park/brothers/runwith_back/domain/Login/service/Version1LoginService.java
+++ src/main/java/park/brothers/runwith_back/domain/Login/service/Version1LoginService.java
@@ -2,7 +2,7 @@ package park.brothers.runwith_back.domain.Login.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import park.brothers.runwith_back.domain.Login.dto.LoginRequestDto;
+import park.brothers.runwith_back.domain.Login.dto.Request.LoginRequestDto;
 import park.brothers.runwith_back.domain.Runner.entity.Runner;
 import park.brothers.runwith_back.domain.Runner.repository.RunnerRepository;
 
