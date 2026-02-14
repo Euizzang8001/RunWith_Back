576f3f8e00f29605f600ad6811549d11759cef6e
Euizzang8001 <qkrdmlcks55@korea.ac.kr>
2026-02-14 15:56:10 +0900

    [test] develop LoginServiceTest - login success test - login fail test

---
+++ src/test/java/park/brothers/runwith_back/domain/Login/service/Version1LoginServiceTest.java
@@ -0,0 +1,69 @@
+package park.brothers.runwith_back.domain.Login.service;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import park.brothers.runwith_back.domain.Login.dto.Request.LoginRequestDto;
+import park.brothers.runwith_back.domain.Runner.entity.Runner;
+import park.brothers.runwith_back.domain.Runner.repository.RunnerRepository;
+
+import java.util.Optional;
+
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.BDDMockito.given;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(MockitoExtension.class)
+class Version1LoginServiceTest {
+
+    @Mock
+    private RunnerRepository runnerRepository;
+
+    @InjectMocks
+    private Version1LoginService loginService;
+
+    @Test
+    @DisplayName("로그인 성공 서비스 테스트")
+    void loginSuccess() {
+        //given
+        //request 선언
+        LoginRequestDto loginRequestDto = new LoginRequestDto(
+                "test_email",
+                "test_password"
+        );
+
+        //레퍼지토리 메서드 수행 시 반환할 값 선언
+        Runner runner = new Runner();
+        runner.setId(1L);
+        runner.setEmail("test_email");
+        runner.setPassword("test_password");
+        runner.setName("test_name");
+
+        //이메일로 찾을 시, 잘 찾아지는지 확인
+        given(runnerRepository.findByEmail(anyString())).willReturn(Optional.of(runner));
+
+        //then
+        assertThat(loginService.login(loginRequestDto)).isEqualTo(1L); //Id가 잘 반환되는지 확인
+    }
+
+    @Test
+    @DisplayName("로그인 실패 서비스 테스트")
+    void loginFail() {
+        //given
+        //request 선언
+        LoginRequestDto loginRequestDto = new LoginRequestDto(
+                "test_email",
+                "test_password"
+        );
+
+        //이메일로 찾을 시, 찾아지는 값이 없도록 함
+        given(runnerRepository.findByEmail(anyString())).willReturn(Optional.empty());
+
+        //then
+        assertThat(loginService.login(loginRequestDto)).isNull(); //null값이 반환되는지 확인
+    }
+}
