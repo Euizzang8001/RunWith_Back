196015c3772aecaaf51c4045d988d6aa5c9b343c
Euizzang8001 <qkrdmlcks55@korea.ac.kr>
2026-02-16 19:44:25 +0900

    [test] Write GroupController Test

---
+++ src/test/java/park/brothers/runwith_back/domain/Group/controller/GroupControllerTest.java
@@ -0,0 +1,215 @@
+package park.brothers.runwith_back.domain.Group.controller;
+
+import org.jspecify.annotations.NonNull;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import park.brothers.runwith_back.domain.Group.dto.Request.CreateGroupRequestDto;
+import park.brothers.runwith_back.domain.Group.dto.Request.DeleteGroupRequestDto;
+import park.brothers.runwith_back.domain.Group.dto.Request.ReviseGroupRequestDto;
+import park.brothers.runwith_back.domain.Group.dto.Response.CreateGroupResponseDto;
+import park.brothers.runwith_back.domain.Group.dto.Response.GetGroupResponseDto;
+import park.brothers.runwith_back.domain.Group.dto.Response.ReviseGroupResponseDto;
+import park.brothers.runwith_back.domain.Group.service.GroupService;
+import tools.jackson.databind.ObjectMapper;
+
+import java.util.List;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@ExtendWith(MockitoExtension.class)
+class GroupControllerTest {
+
+    private MockMvc mockMvc;
+
+    @Mock
+    private GroupService groupService;
+
+    @InjectMocks
+    private GroupController groupController;
+
+    @BeforeEach
+    void setUp() {
+        mockMvc = MockMvcBuilders.standaloneSetup(groupController) //스프링 컨텍스트 없이 테스트 수행
+                .alwaysDo(print()) // 모든 요청에 대해 로그 출력
+                .build();
+    }
+
+
+    @Test
+    @DisplayName("그룹 객체 저장 성공 테스트")
+    void save() throws Exception {
+        //given
+        CreateGroupRequestDto createGroupRequestDto = new CreateGroupRequestDto(
+            "test_name",
+                0L,
+                "test_nickname",
+                0,
+                "test_description",
+                "test_imageLink"
+        );
+        CreateGroupResponseDto createGroupResponseDto = new CreateGroupResponseDto(
+                0L,
+                "test_name",
+                "test_description",
+                "test_imageLink"
+        );
+
+        //가정 설정
+        given(groupService.save(any(CreateGroupRequestDto.class))).willReturn(createGroupResponseDto);
+
+        //when & then
+        String content = new ObjectMapper().writeValueAsString(createGroupRequestDto);
+
+        mockMvc.perform(post("/api/v1/groups") // POST 요청 URL
+                        .contentType(MediaType.APPLICATION_JSON) // 요청 타입 확인
+                        .content(content)) // Body에 JSON 문자열 담기
+                .andExpect(status().isCreated()) //
+                .andExpect(jsonPath("$.id").value(0L)) // id 확인
+                .andExpect(jsonPath("$.name").value("test_name")) // 이름 확인
+                .andExpect(jsonPath("$.description").value("test_description"))
+                .andExpect(jsonPath("$.imageLink").value("test_imageLink"));
+
+    }
+
+    @Test
+    @DisplayName("해당 단어가 들어간 이름을 가진 모든 그룹 찾기 성공 컨트롤러 테스트")
+    void findGroupsBySimilarName() throws Exception {
+        //given
+        List<GetGroupResponseDto> resultGroups = getGetGroupResponseDtos();
+        given(groupService.getGroupsBySimilarName(anyString())).willReturn(resultGroups);
+
+        //when & then
+        mockMvc.perform(get("/api/v1/groups/test")) // get요청
+                .andExpect(status().isOk()) //
+                .andExpect(jsonPath("$", hasSize(2))) //전체 길이가 2인지 확인
+                .andExpect(jsonPath("$[0].id").value(1L)) // id 확인
+                .andExpect(jsonPath("$[0].name").value("test_name1")) // 이름 확인
+                .andExpect(jsonPath("$[0].description").value("test_description1"))
+                .andExpect(jsonPath("$[0].imageLink").value("test_imageLink1"))
+                .andExpect(jsonPath("$[1].id").value(2L)) // id 확인
+                .andExpect(jsonPath("$[1].name").value("test_name2")) // 이름 확인
+                .andExpect(jsonPath("$[1].description").value("test_description2"))
+                .andExpect(jsonPath("$[1].imageLink").value("test_imageLink2"));
+
+
+    }
+
+    @Test
+    @DisplayName("모든 그룹 조회 성공 테스트")
+    void getAllGroups() throws Exception {
+        //given
+        List<GetGroupResponseDto> resultGroups = getGetGroupResponseDtos();
+        given(groupService.getAllGroups()).willReturn(resultGroups);
+
+        //when & then
+        mockMvc.perform(get("/api/v1/groups")) // get요청
+                .andExpect(status().isOk()) //
+                .andExpect(jsonPath("$", hasSize(2))) //전체 길이가 2인지 확인
+                .andExpect(jsonPath("$[0].id").value(1L)) // id 확인
+                .andExpect(jsonPath("$[0].name").value("test_name1")) // 이름 확인
+                .andExpect(jsonPath("$[0].description").value("test_description1"))
+                .andExpect(jsonPath("$[0].imageLink").value("test_imageLink1"))
+                .andExpect(jsonPath("$[1].id").value(2L)) // id 확인
+                .andExpect(jsonPath("$[1].name").value("test_name2")) // 이름 확인
+                .andExpect(jsonPath("$[1].description").value("test_description2"))
+                .andExpect(jsonPath("$[1].imageLink").value("test_imageLink2"));
+
+
+    }
+
+    @Test
+    @DisplayName("그룹 삭제 성공 테스트")
+    void deleteGroup() throws Exception {
+        //given
+        DeleteGroupRequestDto deleteGroupRequestDto = new DeleteGroupRequestDto(
+          0L,
+          0L
+        );
+
+        //when & then
+        //JSON형식의 문자열로 반환
+        String content = new ObjectMapper().writeValueAsString(deleteGroupRequestDto);
+
+        mockMvc.perform(delete("/api/v1/groups") // DELETE 요청 URL
+                        .contentType(MediaType.APPLICATION_JSON) // 요청 타입 확인
+                        .content(content)) // Body에 JSON 문자열 담기
+                .andExpect(status().isOk()) //
+                .andExpect(jsonPath("$.message").value("그룹이 성공적으로 삭제되었습니다."));
+    }
+
+    @Test
+    @DisplayName("그룹 정보 수정 성공 컨트롤러 테스트")
+    void reviseGroupInfo() throws Exception {
+        ReviseGroupRequestDto reviseGroupRequestDto = new ReviseGroupRequestDto(
+                0L,
+                0L,
+                0,
+                "test_description",
+                "test_imageLink"
+        );
+        ReviseGroupResponseDto reviseGroupResponseDto = new ReviseGroupResponseDto(
+                0L,
+                "test_revised_name",
+                "test_revised_description",
+                "test_revised_imageLink",
+                0
+        );
+
+        given(groupService.reviseGroup(any(ReviseGroupRequestDto.class))).willReturn(reviseGroupResponseDto);
+
+        //when & then
+        //JSON형식의 문자열로 반환
+        String content = new ObjectMapper().writeValueAsString(reviseGroupRequestDto);
+
+        mockMvc.perform(patch("/api/v1/groups") // DELETE 요청 URL
+                        .contentType(MediaType.APPLICATION_JSON) // 요청 타입 확인
+                        .content(content)) // Body에 JSON 문자열 담기
+                .andExpect(status().isOk()) //
+                .andExpect(jsonPath("$.id").value(0L))
+                .andExpect(jsonPath("$.name").value("test_revised_name"))
+                .andExpect(jsonPath("$.description").value("test_revised_description"))
+                .andExpect(jsonPath("$.imageLink").value("test_revised_imageLink"));
+    }
+
+    // 중복 코드 메서드화(GetGroupResponseDto로 이루어진 리스트 만들기)
+    private static @NonNull List<GetGroupResponseDto> getGetGroupResponseDtos() {
+        GetGroupResponseDto getGroupResponseDto1 = new GetGroupResponseDto(
+                1L,
+                "test_name1",
+                "test_description1",
+                "test_imageLink1"
+        );
+        GetGroupResponseDto getGroupResponseDto2 = new GetGroupResponseDto(
+                2L,
+                "test_name2",
+                "test_description2",
+                "test_imageLink2"
+        );
+        return List.of(
+                getGroupResponseDto1,
+                getGroupResponseDto2
+        );
+    }
+}
