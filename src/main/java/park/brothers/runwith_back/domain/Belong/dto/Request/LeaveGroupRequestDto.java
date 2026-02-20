24ba2c2a27c2e870e43c3cec88c355ab958b1c5a
Euizzang8001 <qkrdmlcks55@korea.ac.kr>
2026-02-21 01:18:21 +0900

    [refactor] Delete unused import

--- src/main/java/park/brothers/runwith_back/domain/Belong/dto/Request/LeaveGroupRequestDto.java
+++ src/main/java/park/brothers/runwith_back/domain/Belong/dto/Request/LeaveGroupRequestDto.java
@@ -1,7 +1,6 @@
 package park.brothers.runwith_back.domain.Belong.dto.Request;
 
 import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
