626860469b79d1209d5b2637d6aafd239b0b9acb
Euizzang8001 <qkrdmlcks55@korea.ac.kr>
2026-02-21 01:28:58 +0900

    [refactor] Delete unused import

--- src/main/java/park/brothers/runwith_back/domain/Schedule/dto/Request/ReviseScheduleRequestDto.java
+++ src/main/java/park/brothers/runwith_back/domain/Schedule/dto/Request/ReviseScheduleRequestDto.java
@@ -1,7 +1,6 @@
 package park.brothers.runwith_back.domain.Schedule.dto.Request;
 
 import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import lombok.Getter;
 
