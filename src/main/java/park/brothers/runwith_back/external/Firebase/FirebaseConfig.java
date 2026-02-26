d53b69e37b9e92398d69b363d9c58e03e5d72dcf
Euizzang8001 <qkrdmlcks55@korea.ac.kr>
2026-02-26 21:22:44 +0900

    [build] firebaseKey.json을 base64로 인코딩하여, 배포 과정 중 드러나지 않도록 하여 ci/cd 진행

--- src/main/java/park/brothers/runwith_back/external/Firebase/FirebaseConfig.java
+++ src/main/java/park/brothers/runwith_back/external/Firebase/FirebaseConfig.java
@@ -3,21 +3,30 @@ package park.brothers.runwith_back.external.Firebase;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.Base64;
 
 @Configuration //Config 파일임을 선언 -> 앱 시작 시 파이어베이스와 연결
 public class FirebaseConfig {
 
+    @Value("${firebase.base.key}") // application.yml에서 읽어옴
+    private String firebaseBaseKey;
+
     @Bean
     public FirebaseApp initializeFirebase() throws IOException {
-        System.out.println("new ClassPathResource(\"firebaseKey.json\") = " + new ClassPathResource("firebaseKey.json"));
-        //firebase-key.json을 읽어와, 파이어베이스에 서버임을 인증
+        //firebaseBaseKey를 읽어와, Base64로 인코딩한 후, 파이어베이스에 서버임을 인증하는데 사용
+        String base64EncodedServiceAccountKey = firebaseBaseKey;
+
+        InputStream credentialsStream = new ByteArrayInputStream(Base64.getDecoder().decode(base64EncodedServiceAccountKey));
+
         FirebaseOptions options = FirebaseOptions.builder()
-                .setCredentials(GoogleCredentials.fromStream(new ClassPathResource("firebaseKey.json").getInputStream()))
+                .setCredentials(GoogleCredentials.fromStream(credentialsStream))
                 .build();
 
         //파이어베이스의 앱이 없으면 시작하고, 있으면 인스턴스 가져오기
