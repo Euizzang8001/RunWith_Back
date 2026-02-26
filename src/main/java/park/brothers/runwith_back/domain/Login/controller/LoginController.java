9073da7794262dfd013b7d7816c48d6a11aa978e
Euizzang8001 <qkrdmlcks55@korea.ac.kr>
2026-02-26 21:44:27 +0900

    [refactor] 불필요한 login 관련 코드 주석 처리

--- src/main/java/park/brothers/runwith_back/domain/Login/controller/LoginController.java
+++ src/main/java/park/brothers/runwith_back/domain/Login/controller/LoginController.java
@@ -1,64 +1,64 @@
-package park.brothers.runwith_back.domain.Login.controller;
-
-import jakarta.servlet.http.Cookie;
-import jakarta.servlet.http.HttpServletResponse;
-import jakarta.validation.Valid;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
-import park.brothers.runwith_back.common.CommonMessage;
-import park.brothers.runwith_back.common.Response.ValidationErrorUtils;
-import park.brothers.runwith_back.domain.Login.dto.Request.LoginRequestDto;
-import park.brothers.runwith_back.domain.Login.dto.Response.LoginResponseDto;
-import park.brothers.runwith_back.domain.Login.service.LoginService;
-
-@Slf4j
-@RestController
-@RequiredArgsConstructor
-public class LoginController {
-
-    private final LoginService loginService;
-
-    //login api
-    @PostMapping("/api/v1/runners/login")
-    public ResponseEntity<Object> login(@RequestBody @Valid LoginRequestDto loginRequestDto,
-                                        BindingResult bindingResult,
-                                        HttpServletResponse response) {
-
-        if (bindingResult.hasErrors()) {
-            return ValidationErrorUtils.handleValidationErrors(bindingResult);
-        }
-
-        //로그인
-        LoginResponseDto loginResponseDto = loginService.login(loginRequestDto);
-
-        //Http 쿠키 설정
-        Cookie idCookie = new Cookie("runnerId", loginResponseDto.getRunnerId());
-        idCookie.setPath("/");
-        idCookie.setHttpOnly(true);
-        response.addCookie(idCookie);
-
-        return ResponseEntity.status(HttpStatus.OK).body(loginResponseDto);
-    }
-
-
-    // logout api
-    @PostMapping("/api/v1/runners/logout")
-    public ResponseEntity<Object> logout(HttpServletResponse response){
-        expireCookie(response);
-        return ResponseEntity.status(HttpStatus.OK).body(new CommonMessage("로그아웃 성공"));
-
-    }
-
-    private void expireCookie(HttpServletResponse response){
-        Cookie cookie = new Cookie("runnerId", null);
-        cookie.setMaxAge(0);
-        cookie.setPath("/");
-        response.addCookie(cookie);
-    }
-}
+//package park.brothers.runwith_back.domain.Login.controller;
+//
+//import jakarta.servlet.http.Cookie;
+//import jakarta.servlet.http.HttpServletResponse;
+//import jakarta.validation.Valid;
+//import lombok.RequiredArgsConstructor;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.validation.BindingResult;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestBody;
+//import org.springframework.web.bind.annotation.RestController;
+//import park.brothers.runwith_back.common.CommonMessage;
+//import park.brothers.runwith_back.common.Response.ValidationErrorUtils;
+//import park.brothers.runwith_back.domain.Login.dto.Request.LoginRequestDto;
+//import park.brothers.runwith_back.domain.Login.dto.Response.LoginResponseDto;
+//import park.brothers.runwith_back.domain.Login.service.LoginService;
+//
+//@Slf4j
+//@RestController
+//@RequiredArgsConstructor
+//public class LoginController {
+//
+//    private final LoginService loginService;
+//
+//    //login api
+//    @PostMapping("/api/v1/runners/login")
+//    public ResponseEntity<Object> login(@RequestBody @Valid LoginRequestDto loginRequestDto,
+//                                        BindingResult bindingResult,
+//                                        HttpServletResponse response) {
+//
+//        if (bindingResult.hasErrors()) {
+//            return ValidationErrorUtils.handleValidationErrors(bindingResult);
+//        }
+//
+//        //로그인
+//        LoginResponseDto loginResponseDto = loginService.login(loginRequestDto);
+//
+//        //Http 쿠키 설정
+//        Cookie idCookie = new Cookie("runnerId", loginResponseDto.getRunnerId());
+//        idCookie.setPath("/");
+//        idCookie.setHttpOnly(true);
+//        response.addCookie(idCookie);
+//
+//        return ResponseEntity.status(HttpStatus.OK).body(loginResponseDto);
+//    }
+//
+//
+//    // logout api
+//    @PostMapping("/api/v1/runners/logout")
+//    public ResponseEntity<Object> logout(HttpServletResponse response){
+//        expireCookie(response);
+//        return ResponseEntity.status(HttpStatus.OK).body(new CommonMessage("로그아웃 성공"));
+//
+//    }
+//
+//    private void expireCookie(HttpServletResponse response){
+//        Cookie cookie = new Cookie("runnerId", null);
+//        cookie.setMaxAge(0);
+//        cookie.setPath("/");
+//        response.addCookie(cookie);
+//    }
+//}
--- src/main/java/park/brothers/runwith_back/domain/Login/service/Version1LoginService.java
+++ src/main/java/park/brothers/runwith_back/domain/Login/service/Version1LoginService.java
@@ -1,37 +1,37 @@
-package park.brothers.runwith_back.domain.Login.service;
-
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-import park.brothers.runwith_back.common.Exceptions.ResourceNotFoundException;
-import park.brothers.runwith_back.domain.Login.dto.Request.LoginRequestDto;
-import park.brothers.runwith_back.domain.Login.dto.Response.LoginResponseDto;
-import park.brothers.runwith_back.domain.Runner.entity.Runner;
-import park.brothers.runwith_back.domain.Runner.repository.RunnerRepository;
-import park.brothers.runwith_back.external.AWS_S3.AWSS3Service;
-
-import java.util.Optional;
-
-@Service
-@RequiredArgsConstructor
-public class Version1LoginService implements LoginService {
-
-    private final RunnerRepository runnerRepository;
-
-    private final AWSS3Service awss3Service;
-
-    public LoginResponseDto login(LoginRequestDto loginRequestDto) {
-        Optional<Runner> runner = runnerRepository.findByEmail(loginRequestDto.getLoginEmail())
-                .filter(r -> r.getPassword().equals(loginRequestDto.getLoginPassword()));
-
-        //runner가 있으면 로그인 성공
-        if(runner.isEmpty()){
-            throw new ResourceNotFoundException("이메일 또는 비밀번호가 다릅니다.");
-        }
-        return new LoginResponseDto(
-                runner.get().getId().toString(),
-                runner.get().getName(),
-                awss3Service.getImagePresignedUrl("runners", runner.get().getId().toString(), 0)
-        );
-    }
-
-}
+//package park.brothers.runwith_back.domain.Login.service;
+//
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.stereotype.Service;
+//import park.brothers.runwith_back.common.Exceptions.ResourceNotFoundException;
+//import park.brothers.runwith_back.domain.Login.dto.Request.LoginRequestDto;
+//import park.brothers.runwith_back.domain.Login.dto.Response.LoginResponseDto;
+//import park.brothers.runwith_back.domain.Runner.entity.Runner;
+//import park.brothers.runwith_back.domain.Runner.repository.RunnerRepository;
+//import park.brothers.runwith_back.external.AWS_S3.AWSS3Service;
+//
+//import java.util.Optional;
+//
+//@Service
+//@RequiredArgsConstructor
+//public class Version1LoginService implements LoginService {
+//
+//    private final RunnerRepository runnerRepository;
+//
+//    private final AWSS3Service awss3Service;
+//
+//    public LoginResponseDto login(LoginRequestDto loginRequestDto) {
+//        Optional<Runner> runner = runnerRepository.findByEmail(loginRequestDto.getLoginEmail())
+//                .filter(r -> r.getPassword().equals(loginRequestDto.getLoginPassword()));
+//
+//        //runner가 있으면 로그인 성공
+//        if(runner.isEmpty()){
+//            throw new ResourceNotFoundException("이메일 또는 비밀번호가 다릅니다.");
+//        }
+//        return new LoginResponseDto(
+//                runner.get().getId().toString(),
+//                runner.get().getName(),
+//                awss3Service.getImagePresignedUrl("runners", runner.get().getId().toString(), 0)
+//        );
+//    }
+//
+//}
